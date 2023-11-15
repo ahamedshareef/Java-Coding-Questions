8e175b8ecb78847be07cc9d9253bc09cc93b22f2
ahamedshareef <shareef.006@gmail.com>
2023-11-15 20:32:37 +0530

    AddedNew Files

--- leetcode/src/main/java/medium/SumOfThree15.java
+++ leetcode/src/main/java/medium/SumOfThree15.java
@@ -11,38 +11,37 @@ public class SumOfThree15 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-threeSum(new int[] {-1,0,1,2,-1,-4});
+		threeSum(new int[] { -1, 0, 1, 2, -1, -4 });
 	}
-	
-	
-	 public static List<List<Integer>> threeSum(int[] nums) {
-		 int target=0;
-		 List<List<Integer>> threeSum = new ArrayList<>();
-		 Set<List<Integer>> set = new HashSet<>();
-		 Arrays.sort(nums);
-		 
-		 for(int i=0;i<nums.length-2;i++) {
-			
-			 int l=i+1;
-			 int r=nums.length-1;
-			 while(l<r) {
-				 int sum = nums[i]+nums[l]+nums[r];
-				 
-				 if(sum==target) {
-					 set.add(Arrays.asList(nums[i],nums[l],nums[r]));
-					 l++;r--;
-				 }else if(sum<target) {
-					 l++;
-				 }else if(target<sum) {
-					 r--;
-				 }
-			 }
-		 }
-		 
-		 threeSum.addAll(set);
-		 return threeSum;
-	        
-	    }
-	 
+
+	public static List<List<Integer>> threeSum(int[] nums) {
+		int target = 0;
+		List<List<Integer>> threeSum = new ArrayList<>();
+		Set<List<Integer>> set = new HashSet<>();
+		Arrays.sort(nums);
+
+		for (int i = 0; i < nums.length - 2; i++) {
+
+			int l = i + 1;
+			int r = nums.length - 1;
+			while (l < r) {
+				int sum = nums[i] + nums[l] + nums[r];
+
+				if (sum == target) {
+					set.add(Arrays.asList(nums[i], nums[l], nums[r]));
+					l++;
+					r--;
+				} else if (sum < target) {
+					l++;
+				} else if (target < sum) {
+					r--;
+				}
+			}
+		}
+
+		threeSum.addAll(set);
+		return threeSum;
+
+	}
 
 }
--- leetcode/src/main/java/medium/TwoSum167.java
+++ leetcode/src/main/java/medium/TwoSum167.java
@@ -4,24 +4,23 @@ public class TwoSum167 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] numbers= {2,7,11,15};
-		int target =9;
+		int[] numbers = { 2, 7, 11, 15 };
+		int target = 9;
 		twoSum(numbers, target);
 
 	}
-	
-	
-	 public static int[] twoSum(int[] numbers, int target) {
-	        int l=0,r=numbers.length-1;
-	        
-	        while(numbers[l] + numbers[r] != target) {
-	        	if(numbers[l]+ numbers[r]< target) l++;
-	        	else 
-	        		r--;
-	        }
-	        
-	        
-	        return new int[] {l+1,r+1};
-	    }
+
+	public static int[] twoSum(int[] numbers, int target) {
+		int l = 0, r = numbers.length - 1;
+
+		while (numbers[l] + numbers[r] != target) {
+			if (numbers[l] + numbers[r] < target)
+				l++;
+			else
+				r--;
+		}
+
+		return new int[] { l + 1, r + 1 };
+	}
 
 }
