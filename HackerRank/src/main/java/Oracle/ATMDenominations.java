06b7b765e4e1749d63a1449d99069cf0f1385dde
ahamedshareef <shareef.006@gmail.com>
2023-11-15 20:42:02 +0530

    ATM denominations Skeleton

---
+++ HackerRank/src/main/java/Oracle/ATMDenominations.java
@@ -0,0 +1,26 @@
+package Oracle;
+
+public class ATMDenominations {
+	
+	/*
+	 * Assume there are 100, 500, 1000 rupee denominations available in an ATM in limited number. 
+	 * Write a program to dispense minimum number of notes 
+	 * for each transaction and you are allowed to dispense maximum of 40 notes per transaction.
+	 * Examples :
+
+Input : [[100, 2], [500, 3], [1000, 4]], 4000
+Output : 4
+
+Input : [[100, 2], [500, 3], [1000, 5]], 10000
+Output : "cannot dispense"
+
+After completing the coding part he tested the code with some test cases and promoted me to third round which will start in another 10 mins.
+
+	 */
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
